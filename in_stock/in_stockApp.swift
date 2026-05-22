6b6d50e02834652b45ecff577b272f53d21c9fa5
LiangSS21118 <seven20051008@gmail.com>
2026-05-22 15:29:39 +0800

    Initial Commit

---
+++ in_stock/in_stockApp.swift
@@ -0,0 +1,17 @@
+//
+//  in_stockApp.swift
+//  in_stock
+//
+//  Created by user0779 on 2026/5/22.
+//
+
+import SwiftUI
+
+@main
+struct in_stockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
